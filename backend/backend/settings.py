8422465ed72f0f896e104be1f4947dcb126ae2d7
KulaginIA <kulagin.ia@mail.ru>
2026-08-10 22:56:34 +0300

    fixed settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,5 +1,6 @@
-from pathlib import Path
 import os
+from pathlib import Path
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -64,10 +65,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('POSTGRES_DB', 'django'),
-	'USER': os.getenv('POSTGRES_USER', 'django'),
-	'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'django'),
-	'HOST': os.getenv('DB_HOST', ''),
-	'POST': os.getenv('DB_PORT', 5432)
+        'USER': os.getenv('POSTGRES_USER', 'django'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'django'),
+        'HOST': os.getenv('DB_HOST', ''),
+        'POST': os.getenv('DB_PORT', 5432)
     }
 }
 
